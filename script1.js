f12e87c4d631386fbfc8454ad04c9d1af7243228
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2024-10-26 01:34:59 +0600

    "Updated index.html, script.js, and style.css with minor changes to HTML structure, event listeners, and CSS styles."

--- script1.js
+++ script1.js
@@ -2,31 +2,28 @@ const resumeLists = document.querySelectorAll('.resume-list');
 const resumeBoxs = document.querySelectorAll('.resume-box');
 
 const portfolioLists = document.querySelectorAll('.portfolio-list');
-const portfolioBoxs = document.querySelectorAll('.portfolio-box');
 
-// navbar action and all section actions along with cube rotation when navbar is clicked
-
-// resume section when clicking tab-list
+// Resume section when clicking tab-list
 resumeLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
+        // Remove the active class from the currently active resume list and box
         document.querySelector('.resume-list.active').classList.remove('active');
-        list.classList.add('active');
-
         document.querySelector('.resume-box.active').classList.remove('active');
+        
+        // Add the active class to the clicked resume list and the corresponding box
+        list.classList.add('active');
         resumeBoxs[idx].classList.add('active');
     });
 });
 
-// portfolio section when clicking tab-list
-portfolioLists.forEach((list, idx)=> {
+// Portfolio section when clicking tab-list
+portfolioLists.forEach((list, idx) => {
     list.addEventListener('click', () => {
+        // Remove the active class from the currently active portfolio list
         document.querySelector('.portfolio-list.active').classList.remove('active');
+        
+        // Add the active class to the clicked portfolio list
         list.classList.add('active');
-
-        document.querySelector('.portfolio-box.active').classList.remove('active');
-        portfolioBoxs[idx].classList.add('active');
     });
-})
-
-
-// visibility for contact section when reloading (cube reloading animation)
+});
+    
